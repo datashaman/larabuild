188b9bf27ac16a1d5c961a17306f2a9e4e4c6db5
Marlin Forbes <marlinf@datashaman.com>
2019-02-23 17:22:25 +0200

    move tests into functional groupings

--- tests/Feature/ProjectQueriesTest.php
+++ tests/Feature/ProjectQueriesTest.php
@@ -8,7 +8,107 @@ use Tests\PassportTestCase;
 
 class ProjectQueriesTest extends PassportTestCase
 {
-    public function testProjectBuilds()
+    public function testProjectsQuery()
+    {
+        $projects = factory(Project::class, 12)
+            ->create()
+            ->take(10)
+            ->map(
+                function ($project) {
+                    return [
+                        'id' => (string) $project->id,
+                        'team' => [
+                            'id' => (string) $project->team->id,
+                            'name' => $project->team->name,
+                            'created_at' => $this->formatDateTime($project->team->created_at),
+                            'updated_at' => $this->formatDateTime($project->team->updated_at),
+                        ],
+                        'name' => $project->name,
+                        'repository' => $project->repository,
+                        'created_at' => $this->formatDateTime($project->created_at),
+                        'updated_at' => $this->formatDateTime($project->updated_at),
+                    ];
+                }
+            )
+            ->all();
+
+        $expected = [
+            'data' => [
+                'projects' => [
+                    'data' => $projects,
+                    'paginatorInfo' => [
+                        'count' => 10,
+                        'currentPage' => 1,
+                        'lastPage' => 2,
+                        'total' => 12,
+                    ],
+                ],
+            ],
+        ];
+
+        $this
+            ->postJson(
+                '/graphql',
+                [
+                    'query' => '{
+                        projects(count: 10) {
+                            paginatorInfo {
+                                count
+                                currentPage
+                                lastPage
+                                total
+                            }
+                            data {
+                                id
+                                team {
+                                    id
+                                    name
+                                    created_at
+                                    updated_at
+                                }
+                                name
+                                repository
+                                created_at
+                                updated_at
+                            }
+                        }
+                    }',
+                ]
+            )
+            ->assertStatus(200)
+            ->assertExactJson($expected);
+    }
+
+    public function testProjectQuery()
+    {
+        $project = factory(Project::class)->create();
+
+        $expected = [
+            'data' => [
+                'project' => [
+                    'id' => (string) $project->id,
+                    'name' => $project->name,
+                ],
+            ],
+        ];
+
+        $this
+            ->postJson(
+                '/graphql',
+                [
+                    'query' => "{
+                        project(id: {$project->id}) {
+                            id
+                            name
+                        }
+                    }",
+                ]
+            )
+            ->assertStatus(200)
+            ->assertExactJson($expected);
+    }
+
+    public function testProjectBuildsQuery()
     {
         $otherBuild = factory(Build::class)->create();
         $project = factory(Project::class)->create();
--- tests/Feature/TeamQueriesTest.php
+++ tests/Feature/TeamQueriesTest.php
@@ -11,6 +11,88 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TeamQueriesTest extends PassportTestCase
 {
+    public function testTeamsQuery()
+    {
+        $teams = factory(Team::class, 12)
+            ->create()
+            ->take(10)
+            ->map(
+                function ($team) {
+                    return [
+                        'id' => (string) $team->id,
+                        'name' => $team->name,
+                    ];
+                }
+            )
+            ->all();
+
+        $expected = [
+            'data' => [
+                'teams' => [
+                    'data' => $teams,
+                    'paginatorInfo' => [
+                        'count' => 10,
+                        'currentPage' => 1,
+                        'lastPage' => 2,
+                        'total' => 12,
+                    ],
+                ],
+            ],
+        ];
+
+        $this
+            ->postJson(
+                '/graphql',
+                [
+                    'query' => '{
+                        teams(count: 10) {
+                            paginatorInfo {
+                                count
+                                currentPage
+                                lastPage
+                                total
+                            }
+                            data {
+                                id
+                                name
+                            }
+                        }
+                    }',
+                ]
+            )
+            ->assertStatus(200)
+            ->assertExactJson($expected);
+    }
+
+    public function testTeamQuery()
+    {
+        $team = factory(Team::class)->create();
+
+        $expected = [
+            'data' => [
+                'team' => [
+                    'id' => (string) $team->id,
+                    'name' => $team->name,
+                ],
+            ],
+        ];
+
+        $this
+            ->postJson(
+                '/graphql',
+                [
+                    'query' => "{
+                        team(id: {$team->id}) {
+                            id
+                            name
+                        }
+                    }",
+                ]
+            )
+            ->assertStatus(200)
+            ->assertExactJson($expected);
+    }
+
     public function testTeamProjectsQuery()
     {
         $otherProject = factory(Project::class);
