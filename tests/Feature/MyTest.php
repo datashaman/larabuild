90b33dde5d312a6ba00b6bb4da4b38a6525897c6
Marlin Forbes <marlinf@datashaman.com>
2019-02-23 16:26:01 +0200

    add my teams query and test

--- tests/Feature/MyTest.php
+++ tests/Feature/MyTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Build;
+use App\Models\Project;
+use App\Models\Team;
 use Tests\PassportTestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -29,6 +32,49 @@ class MyTest extends PassportTestCase
                             id
                             name
                             email
+                        }
+                    }",
+                ]
+            )
+            ->assertStatus(200)
+            ->assertExactJson($expected);
+    }
+
+    public function testMyTeamsQuery()
+    {
+        $otherTeam = factory(Team::class)->create();
+
+        $myTeams = factory(Team::class, 3)
+            ->create()
+            ->each(
+                function ($team) {
+                    $team->users()->attach($this->user);
+                }
+            )
+            ->map(
+                function ($team) {
+                    return [
+                        'id' => (string) $team->id,
+                        'name' => (string) $team->name,
+                    ];
+                }
+            )
+            ->all();
+
+        $expected = [
+            'data' => [
+                'myTeams' => $myTeams,
+            ],
+        ];
+
+        $this
+            ->postJson(
+                '/graphql',
+                [
+                    'query' => "{
+                        myTeams {
+                            id
+                            name
                         }
                     }",
                 ]
