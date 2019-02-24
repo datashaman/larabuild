d86d29cefaebd4e260213e81e7f1677381dc23e6
Marlin Forbes <marlinf@datashaman.com>
2019-02-24 02:36:01 +0200

    create projecr mutation done, last one for the night

---
+++ app/Mutations/CreateProject.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mutations;
+
+use App\Models\Project;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Exceptions\AuthorizationException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class CreateProject
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  null  $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
+     * @param  mixed[]  $args The arguments that were passed into the field.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Arbitrary data that is shared between all fields of a single query.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @return mixed
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        $user = auth()->user();
+
+        $inTeam = $user
+            ->teams()
+            ->where('teams.id', array_get($args, 'project.team_id'))
+            ->exists();
+
+        if ($user->hasRole('admin') || $inTeam) {
+            return Project::create($args['project']);
+        }
+
+        throw new AuthorizationException('You are not authorized to access createProject');
+    }
+}
